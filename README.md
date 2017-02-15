529f058dec28296ec4a2142517358a2fd7178046
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-02-15 16:23:30 -0500

    Wrote a readme for this program and created elaborate hashes to contain all possible playing strategies. Made 2 methods to find and puts each strategy based of user input.

---
+++ README.md
@@ -0,0 +1,4 @@
+This program helps a user form a strategy playing blackjack by analyzing the
+cards in the users hand and the face up card of the dealer and suggesting what
+the next best move is. This program uses extensive hashes to organize data and
+rapidly to find the correct strategy for any card groupings.
---
+++ blackjack_advisor.rb
@@ -0,0 +1,113 @@
+what_do_with_hard = {
+  5 => {2=>"H", 3=>"H", 4=>"H", 5=>"H", 6=>"H", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  6 => {
+    2=>"H", 3=>"H", 4=>"H", 5=>"H", 6=>"H", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  7 => {
+    2=>"H", 3=>"H", 4=>"H", 5=>"H", 6=>"H", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  8 => {
+    2=>"H", 3=>"H", 4=>"H", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  9 => {
+    2=>"DH", 3=>"DH", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  10 => {
+    2=>"DH", 3=>"DH", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"DH", 8=>"DH", 9=>"DH", 10=>"H", 0=>"H"
+  },
+  11 => {
+    2=>"DH", 3=>"DH", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"DH", 8=>"DH", 9=>"DH", 10=>"DH", 0=>"DH"
+  },
+  12 => {
+    2=>"H", 3=>"H", 4=>"S", 5=>"S", 6=>"S", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  13 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  14 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  15 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  16 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  17 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+  18 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+  19 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+  20 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  }
+}
+
+what_to_do_with_soft = {
+  13 => {
+    2=>"H", 3=>"H", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  14 => {
+    2=>"H", 3=>"H", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  15 => {
+    2=>"H", 3=>"H", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  16 => {
+    2=>"H", 3=>"H", 4=>"H", 5=>"DH", 6=>"DH", 7=>"DH", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  17 => {
+    2=>"DH", 3=>"DH", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  18 => {
+    2=>"S", 3=>"DS", 4=>"DS", 5=>"DS", 6=>"DS", 7=>"S", 8=>"S", 9=>"H", 10=>"H", 0=>"S"
+  },
+  19 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"DH", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+  20 => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+}
+
+
+def return_strategy_for_hard_hand(user_total, house_card, strategy)
+  solution = strategy[user_total][house_card]
+end
+
+def return_strategy_for_soft_hand (user_card_1, user_card_2, house_card, strategy)
+  if user_card_1 == "a"
+    user_card_1 = 11
+    user_total = user_card_1 + user_card_2.to_i
+    solution = strategy[user_total][house_card]
+  else
+    user_card_2 = 11
+    user_total = user_card_1.to_i + user_card_2
+    solution = strategy[user_total][house_card]
+  end
+end
+
+
+puts "What are your two cards? Please put 10 for face cards and a for Ace"
+player_first_card = gets.chomp
+player_second_card = gets.chomp
+if player_first_card + player_second_card == "a10" ||player_first_card + player_second_card == "10a"
+  puts "BLACKJACK!"
+  exit
+end
+puts "What is the dealer's card? Please put 10 for face cards and A for Ace"
+dealers_card = gets.chomp.to_i
+if player_first_card == player_second_card
+  pair(player_first_card, dealers_card, what_to_do_with_a_pair) #will add
+elsif player_first_card == "a" || player_second_card == "a"
+  puts return_strategy_for_soft_hand(player_first_card, player_second_card, dealers_card, what_to_do_with_soft)
+else
+  players_total = player_first_card.to_i + player_second_card.to_i
+  puts return_strategy_for_hard_hand(players_total, dealers_card, what_do_with_hard)
+
+end
