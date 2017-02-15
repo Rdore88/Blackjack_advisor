25668acba7e4e005967a77938e7d9907ceab282b
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-02-15 17:13:02 -0500

    added a hash and method to figure out the best game strategy when the user has a matching pair of cards.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -75,20 +75,57 @@ what_to_do_with_soft = {
   },
 }
 
+what_to_do_with_a_pair = {
+  "2" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  "3" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"P", 9=>"H", 10=>"H", 0=>"H"
+  },
+  "4" => {
+    2=>"H", 3=>"H", 4=>"P", 5=>"P", 6=>"P", 7=>"H", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  "5" => {
+    2=>"DH", 3=>"DH", 4=>"DH", 5=>"DH", 6=>"DH", 7=>"DH", 8=>"DH", 9=>"DH", 10=>"H", 0=>"H"
+  },
+  "6" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"H", 9=>"H", 10=>"H", 0=>"H"
+  },
+  "7" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"P", 9=>"H", 10=>"H", 0=>"H"
+  },
+  "8" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"P", 9=>"P", 10=>"P", 0=>"P"
+  },
+  "9" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"S", 8=>"P", 9=>"P", 10=>"S", 0=>"S"
+  },
+  "10" => {
+    2=>"S", 3=>"S", 4=>"S", 5=>"S", 6=>"S", 7=>"S", 8=>"S", 9=>"S", 10=>"S", 0=>"S"
+  },
+  "a" => {
+    2=>"P", 3=>"P", 4=>"P", 5=>"P", 6=>"P", 7=>"P", 8=>"P", 9=>"P", 10=>"P", 0=>"P"
+  },
+
+}
+
+def return_strategy_for_pair(user_card, house_card, strategy)
+  strategy[user_card][house_card]
+end
 
 def return_strategy_for_hard_hand(user_total, house_card, strategy)
-  solution = strategy[user_total][house_card]
+  strategy[user_total][house_card]
 end
 
 def return_strategy_for_soft_hand (user_card_1, user_card_2, house_card, strategy)
   if user_card_1 == "a"
     user_card_1 = 11
     user_total = user_card_1 + user_card_2.to_i
-    solution = strategy[user_total][house_card]
+    strategy[user_total][house_card]
   else
     user_card_2 = 11
     user_total = user_card_1.to_i + user_card_2
-    solution = strategy[user_total][house_card]
+    strategy[user_total][house_card]
   end
 end
 
@@ -100,10 +137,10 @@ if player_first_card + player_second_card == "a10" ||player_first_card + player_
   puts "BLACKJACK!"
   exit
 end
-puts "What is the dealer's card? Please put 10 for face cards and A for Ace"
+puts "What is the dealer's card? Please put 10 for face cards and a for Ace"
 dealers_card = gets.chomp.to_i
 if player_first_card == player_second_card
-  pair(player_first_card, dealers_card, what_to_do_with_a_pair) #will add
+  puts return_strategy_for_pair(player_first_card, dealers_card, what_to_do_with_a_pair)
 elsif player_first_card == "a" || player_second_card == "a"
   puts return_strategy_for_soft_hand(player_first_card, player_second_card, dealers_card, what_to_do_with_soft)
 else
